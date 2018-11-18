0b924d22967cc987611b8190806c030e2481dc69
Zachariasz <zah3@o2.pl>
2018-11-18 08:23:33 +0100

    [UserCollection] create

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class UserResource extends ResourceCollection
+class UserCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
@@ -16,8 +16,7 @@ class UserResource extends ResourceCollection
     {
         return [
             'username' => $this->username,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at
+            'created_at' => $this->created_at
         ];
     }
 }
