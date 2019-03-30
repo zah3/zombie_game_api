cd51543c8e76133f9a6592e0adfb6c2edfb9f1ed
Zachariasz <zah3@o2.pl>
2019-03-30 08:13:15 +0100

    [CharacterRepositoryTest] creates test for actuall methods

---
+++ tests/Feature/Repositories/CharacterRepositoryTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zachariasz
+ * Date: 2019-03-30
+ * Time: 07:41
+ */
+
+namespace Tests\Feature\Repositories;
+
+
+use App\Character;
+use App\User;
+use App\Repositories\CharacterRepository;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CharacterRepositoryTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testSave()
+    {
+        $user = factory(User::class)->create();
+        $character = factory(Character::class)->make();
+        $createdModel = CharacterRepository::create($user, null, $character->name, null);
+        $this->assertNotNull($createdModel);
+        $this->assertEquals($character->name, $createdModel->name);
+        $this->assertDatabaseHas($character->getTable(), $createdModel->toArray());
+    }
+
+    public function testUpdate()
+    {
+        $character = factory(Character::class)->create();
+        $newFields = factory(Character::class)->make();
+        $updatedModel = CharacterRepository::update($character, $newFields->toArray());
+        $this->assertNotNull($updatedModel);
+        $this->assertEquals($updatedModel->name, $newFields->name);
+        $this->assertDatabaseHas($character->getTable(), $updatedModel->toArray());
+    }
+
+    public function testAddExperience()
+    {
+        $character = factory(Character::class)->create();
+        $updatedModel = CharacterRepository::addExperience(100, $character);
+        $character->refresh();
+        $this->assertNotNull($updatedModel);
+        $this->assertEquals(100, $character->experience);
+    }
+}
---
+++ tests/Feature/Services/ExperienceServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zachariasz
+ * Date: 2019-03-30
+ * Time: 07:40
+ */
+
+namespace Tests\Feature\Services;
+
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class ExperienceServiceTest extends TestCase
+{
+    use DatabaseTransactions;
+}
