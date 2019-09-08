ff7164dcb909e1d6b631888fe2a3acbcd93efde0
Zachariasz <zah3@o2.pl>
2019-09-08 11:20:35 +0200

    fixed style in verification notice

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,17 +1,126 @@
 @extends('layouts.app')
+@section('style')
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&amp;subset=latin-ext');
+    html,
+    body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Oswald', sans-serif;
+    font-size: 13px;
+    line-height: 1.4;
+    background: -moz-linear-gradient(-90deg, #f0c27b 0%, #4b1248 100%);
+    background: -webkit-linear-gradient(-90deg, #f0c27b 0%, #4b1248 100%);
+    background: linear-gradient(90deg, #f0c27b 0%, #4b1248 100%);
+    min-height: 100%;
+    height: 100%;
+    display: table;
+    font-weight: 400;
+    text-transform: uppercase;
+    width: 100%;
+    }
+
+    #content {
+    position: relative;
+    min-height: 100%;
+    height: 100%;
+    float: left;
+    width: 100%;
+    padding: 15px 0;
+    }
+
+    #content .desc {
+    display: table;
+    height: 100%;
+    width: 100%;
+    }
+
+    #content .desc .inner {
+    display: table-cell;
+    vertical-align: middle;
+    }
+
+    .app {
+    border: 1px solid rgba(170, 208, 233, .2);
+    box-shadow: 0 16px 28px 0 rgba(0, 0, 0, .22), 0 25px 55px 0 rgba(0, 0, 0, .21);
+    float: left;
+    width: 100%;
+    background: #272727;
+    color: #959AA3;
+    position: relative;
+    }
+
+    .app .heading,
+    .app .footer {
+    text-align: center;
+    width: 100%;
+    }
+
+    .app .footer span {
+    color: rgb(239, 194, 89)
+    }
+
+    .app .heading,
+    .app .main,
+    .app .footer {
+    padding: 15px;
+    }
+
+    .app .main {
+    text-align: center;
+    font-size: 25px;
+    font-weight: 300;
+    }
+
+    .app .main .temp {
+    padding-bottom: 30px;
+    margin-bottom: 30px;
+    }
+
+    .app .main .fa {
+    font-size: 86px;
+    margin-bottom: 15px;
+    display: block;
+    }
+
+    .app .main .temp .fa {
+    font-size: 86px;
+    }
+
+    .app .main .sunrise .fa {
+    font-size: 46px;
+    }
+
+    .app .main .fa,
+    .app .footer span {
+    color: rgb(239, 194, 89)
+    }
+    </style>
+@endsection
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                    <div class="card-body">
-                        <div class="alert alert-{{$type}}" role="alert">
-                            {{ $message }}
+    <div id="content">
+        <div class="desc">
+            <div class="inner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="app">
+                                <div class="heading">
+                                    {{ __('Verify Your Email Address') }}
+                                </div>
+                                <div class="main">
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-md-12 temp" style="color: {{$color}};">
+                                                <i class="fa fa-thermometer-full" aria-hidden="true"></i>
+                                                {{ $message }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
     <!-- Latest compiled and minified CSS -->
     <link
             rel="stylesheet"
@@ -17,50 +16,19 @@
             integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"
             crossorigin="anonymous"
     >
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script
-            src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
-            integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
-            crossorigin="anonymous"
-    >
-
-    </script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+    @yield('style')
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    @yield('content')
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="{{asset('js/bootstrap.min.js')}}" ></script>
+<script>
+    $("#content").css("bottom", $("footer").outerHeight());
+</script>
 </html>
